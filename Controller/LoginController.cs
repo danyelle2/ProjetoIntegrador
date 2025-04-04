4027d3404ac5fe3d4992acca3ce6ac957b3aa2c2
Danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-03 23:16:13 -0300

    Controller criada para validacao de senha login

---
+++ Controller/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoIntegrador.Controller
+{
+    internal class LoginController
+    {
+        public bool RealizarLogin(string usuario, string senha, Label labelMensagem)
+        {
+            if (string.IsNullOrEmpty(usuario) && string.IsNullOrEmpty(senha))
+            {
+                MessageBox.Show("Favor, Preencher todos os campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (usuario != "admin" && senha != "1234")
+            {
+                labelMensagem.Text = "Usuário ou senha incorretos";
+                return false;
+            }
+
+            TelaInicial telaInicial = new TelaInicial();
+            telaInicial.Show();
+            return true;
+
+
+        }
+    }
+}
