af7c8d57fbcbb23b9bad2f68f5281527cbd7e101
Danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-03 23:14:41 -0300

    Tela de login criada

---
+++ View/TelaLoginForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ProjetoIntegrador
+{
+    partial class TelaLoginForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.MsgErro = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSenha = new System.Windows.Forms.TextBox();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(171, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 37);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "StudioV";
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCadastro.Location = new System.Drawing.Point(329, 461);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastro.TabIndex = 23;
+            this.btnCadastro.Text = "Cadastre-se";
+            this.btnCadastro.UseVisualStyleBackColor = true;
+            // 
+            // MsgErro
+            // 
+            this.MsgErro.AutoSize = true;
+            this.MsgErro.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MsgErro.ForeColor = System.Drawing.Color.Red;
+            this.MsgErro.Location = new System.Drawing.Point(306, 336);
+            this.MsgErro.Name = "MsgErro";
+            this.MsgErro.Size = new System.Drawing.Size(19, 15);
+            this.MsgErro.TabIndex = 22;
+            this.MsgErro.Text = "...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(305, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 24);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Senha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(294, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 24);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Digite seu CPF";
+            // 
+            // TxtSenha
+            // 
+            this.TxtSenha.Location = new System.Drawing.Point(298, 313);
+            this.TxtSenha.Name = "TxtSenha";
+            this.TxtSenha.PasswordChar = '*';
+            this.TxtSenha.Size = new System.Drawing.Size(177, 20);
+            this.TxtSenha.TabIndex = 19;
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.Location = new System.Drawing.Point(298, 228);
+            this.TxtUsuario.MaxLength = 11;
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(177, 20);
+            this.TxtUsuario.TabIndex = 18;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.Location = new System.Drawing.Point(487, 461);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 17;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // TelaLoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 568);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCadastro);
+            this.Controls.Add(this.MsgErro);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtSenha);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "TelaLoginForm";
+            this.Text = "login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.TelaLoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.Label MsgErro;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSenha;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
--- View/TelaLoginForm.cs
+++ View/TelaLoginForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace ProjetoIntegrador
 {
-    public partial class Form1 : Form
+    public partial class TelaLoginForm : Form
     {
-        public Form1()
+        public TelaLoginForm()
         {
             InitializeComponent();
+        }
+
+        private void TelaLoginForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
