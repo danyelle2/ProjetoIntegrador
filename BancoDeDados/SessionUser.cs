8040acd9dc6dcd609930ec3ae94fa60a5930b4f2
danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-09 16:23:19 -0300

    Pastas comentadas do banco de dados.

--- BancoDeDados/SessionUser.cs
+++ BancoDeDados/SessionUser.cs
@@ -1,25 +1,25 @@
-﻿using ConexaoMySQL.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using ConexaoMySQL.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ProjetoIntegrador.BancoDeDados
-{
-    public static class SessionUser
-    {
-        public static Usuario userLogado;
+//namespace ProjetoIntegrador.BancoDeDados
+//{
+//    public static class SessionUser
+//    {
+//        public static Usuario userLogado;
 
-        public static Usuario Login(Usuario usuario)
-        {
-            return userLogado = usuario;
-        }
+//        public static Usuario Login(Usuario usuario)
+//        {
+//            return userLogado = usuario;
+//        }
 
-        public static void Logout()
-        {
-            userLogado = null;
-        }
+//        public static void Logout()
+//        {
+//            userLogado = null;
+//        }
 
-    }
-}
+//    }
+//}
--- Model/Usuario.cs
+++ Model/Usuario.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ProjetoIntegrador.Model
-{
-    internal class Usuario
-    { 
-        // colocar do jeito que ta no banco de dados
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Cpf { get; set; }
-        public string SenhaHash { get; set; }
-        
-        
-    }
-}
+//namespace ProjetoIntegrador.Model
+//{
+//    internal class Usuario
+//    {
+//        // colocar do jeito que ta no banco de dados
+//        public int Id { get; set; }
+//        public string Nome { get; set; }
+//        public string Cpf { get; set; }
+//        public string SenhaHash { get; set; }
+
+
+//    }
+//}
