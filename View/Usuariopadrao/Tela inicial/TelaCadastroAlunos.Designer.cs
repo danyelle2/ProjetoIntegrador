b5f480c43f684e0bb52859c17160e72df9a6cf90
amanda-ar44 <amanda.raraujo10@senacsp.edu.br>
2025-04-11 17:24:47 -0300

    Tela Cadastro  -  Iniciada

--- View/Usuariopadrao/Tela inicial/TelaCadastroAlunos.Designer.cs
+++ View/Usuariopadrao/Tela inicial/TelaCadastroAlunos.Designer.cs
@@ -173,9 +173,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(232, 40);
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(234, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(54, 23);
             this.label1.TabIndex = 79;
             this.label1.Text = "Nome";
             // 
