f65d56deddb23bd9f671796088e5419d4f8d9b69
danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-11 17:04:02 -0300

    Comentarios adicionados

--- BancoDeDados/AuthService.cs
+++ BancoDeDados/AuthService.cs
@@ -20,22 +20,22 @@ namespace ProjetoIntegrador.BancoDeDados
         {
             try
             {
-                // Query SQL agora valida o CPF e o status do usuário.
+                //  Fiz essa para autenticar login
                 string query = "SELECT * FROM usuario WHERE cpf = @cpfdigitado AND status_usuario = @StatusUsuario";
                 var parameters = new MySqlParameter[]
                 {
                    new MySqlParameter("@cpfdigitado", cpf),
-                   new MySqlParameter("@StatusUsuario", statusUsuario ? 1 : 0), // Converte o valor de bool para 1 ou 0
+                   new MySqlParameter("@StatusUsuario", statusUsuario ? 1 : 0), 
                 };
 
                 using (var respostaBanco = _databaseService.ExecuteQuery(query, parameters))
                 {
                     if (respostaBanco.Read())
                     {
-                        // Verificando o status do usuário armazenado no banco
-                        bool status = Convert.ToBoolean(respostaBanco["status_usuario"]); // A conversão para booleano
+                        
+                        bool status = Convert.ToBoolean(respostaBanco["status_usuario"]); 
 
-                        // Comparando a senha
+                        
                         string storedHash = respostaBanco["senha"].ToString();
                         string inputHash = Criptografia.HashPassword(senha);
 
@@ -48,18 +48,16 @@ namespace ProjetoIntegrador.BancoDeDados
                                 Senha = respostaBanco["senha"].ToString(),
                                 TipoUsuario = respostaBanco["tipo_usuario"].ToString(),
                                 TipoMembro = Convert.ToInt32(respostaBanco["id_professor"]),
-                                StatusUsuario = status, // Retorna o status convertido corretamente
+                                StatusUsuario = status, 
                             };
                         }
                     }
                 }
 
-                // Retorna null se o usuário não for encontrado ou se as credenciais forem inválidas
                 return null;
             }
             catch (Exception ex)
             {
-                // Retorna um erro detalhado caso algo aconteça durante a autenticação
                 throw new Exception("Erro durante autenticação: " + ex.Message);
             }
         }
