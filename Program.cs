e65c431c539a2c5bdcbf377116ba9322a441803a
amanda-ar44 <amanda.raraujo10@senacsp.edu.br>
2025-04-11 17:02:13 -0300

    Tela de Alterar Dados feita!

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace ProjetoIntegrador
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TelaModalidadeEscolha());
+            Application.Run(new TelaAlterarDadosAlunosForms());
             //Application.Run(new TelaLoginForm());
         }
     }
