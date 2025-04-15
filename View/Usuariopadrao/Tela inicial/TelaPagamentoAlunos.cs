e50af3318b7e247c9c53fa00d2d96e23c48261c3
danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-15 16:30:12 -0300

    comentarios adicionados a tela PagamentoAluno

--- View/Usuariopadrao/Tela inicial/TelaPagamentoAlunos.cs
+++ View/Usuariopadrao/Tela inicial/TelaPagamentoAlunos.cs
@@ -25,14 +25,10 @@ namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
         private void dataGridViewpagamento_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
-            // não sei se coloca nessa tela ou coloca na classe controller e só chamo aqui
-            //teste de funcionamento
+            
             List<Aluno> alunos = new List<Aluno>
         {
-            new Aluno { Nome = "fulano", StatusAtivo = true, StatusPagamento = false },
-            new Aluno { Nome = "teste", StatusAtivo = true, StatusPagamento = true },
-            new Aluno { Nome = "aaaa", StatusAtivo = false, StatusPagamento = false },
-            new Aluno { Nome = "bbbbb", StatusAtivo = true, StatusPagamento = false }
+           
         };
 
             var alunosAtivos = alunos.Where(a => a.StatusAtivo).ToList();
@@ -64,8 +60,7 @@ namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
 
         private void buttonPagamentoRealizado_Click(object sender, EventArgs e)
         {
-            // teste de funcionamento
-            //tem que ver como colocar isso quando tiver o banco de dados
+           
             foreach (DataGridViewRow row in dataGridViewpagamento.SelectedRows)
             {
                 var aluno = (Aluno)row.DataBoundItem;
@@ -78,8 +73,7 @@ namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
         private void buttonPendentePagamento_Click(object sender, EventArgs e)
         {
 
-            // teste de funcionamento
-            //tem que ver como colocar isso quando tiver o banco de dados
+            
             foreach (DataGridViewRow row in dataGridViewpagamento.SelectedRows)
             {
                 var aluno = (Aluno)row.DataBoundItem;
@@ -89,3 +83,77 @@ namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
         }
     }
 }
+// Colocar para esse na tela do código caso não de certo.
+
+//using ProjetoIntegrador.BancoDeDados;
+//using ProjetoIntegrador.Controller.Aluno;
+//using ProjetoIntegrador.Model;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Windows.Forms;
+
+//namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
+//{
+//    public partial class TelaPagamentoAlunos : Form
+//    {
+//        private AlunoPagamentoController alunoController;
+
+//        public TelaPagamentoAlunos()
+//        {
+//            InitializeComponent();
+//            DatabaseService dbService = new DatabaseService(); // Conexão com o banco de dados
+//            alunoController = new AlunoPagamentoController(dbService); // Instancia o controller
+//        }
+
+//        private void TelaPagamentoAlunos_Load(object sender, EventArgs e)
+//        {
+//            CarregarAlunos();  // Carregar os alunos ativos assim que a tela for carregada
+//        }
+
+//        private void CarregarAlunos()
+//        {
+//            // Obtém a lista de alunos ativos do banco de dados
+//            List<Aluno> alunosAtivos = alunoController.AlunosAtivos();
+
+//            // Define o DataSource do DataGridView com os alunos ativos
+//            dataGridViewpagamento.DataSource = alunosAtivos;
+
+//            // Formatação da célula para indicar o status do pagamento
+//            dataGridViewpagamento.CellFormatting += (s, ev) =>
+//            {
+//                if (ev.RowIndex >= 0 && dataGridViewpagamento.Columns[ev.ColumnIndex].Name == "StatusPagamento")
+//                {
+//                    bool statusPagamento = (bool)dataGridViewpagamento.Rows[ev.RowIndex].Cells[ev.ColumnIndex].Value;
+//                    ev.CellStyle.BackColor = statusPagamento ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+//                }
+//            };
+//        }
+
+//        private void buttonPagamentoRealizado_Click(object sender, EventArgs e)
+//        {
+//            // Atualizar o status de pagamento para 'true' (realizado) no banco de dados
+//            foreach (DataGridViewRow row in dataGridViewpagamento.SelectedRows)
+//            {
+//                var aluno = (Aluno)row.DataBoundItem;  // Obtém o aluno selecionado
+//                alunoController.AtualizarStatusPagamento(aluno.Id, true);  // Atualiza o status no banco
+//            }
+
+//            // Recarrega a lista de alunos para refletir as alterações
+//            CarregarAlunos();
+//        }
+
+//        private void buttonPendentePagamento_Click(object sender, EventArgs e)
+//        {
+//            // Atualizar o status de pagamento para 'false' (pendente) no banco de dados
+//            foreach (DataGridViewRow row in dataGridViewpagamento.SelectedRows)
+//            {
+//                var aluno = (Aluno)row.DataBoundItem;  // Obtém o aluno selecionado
+//                alunoController.AtualizarStatusPagamento(aluno.Id, false);  // Atualiza o status no banco
+//            }
+
+//            // Recarrega a lista de alunos para refletir as alterações
+//            CarregarAlunos();
+//        }
+//    }
+//}
