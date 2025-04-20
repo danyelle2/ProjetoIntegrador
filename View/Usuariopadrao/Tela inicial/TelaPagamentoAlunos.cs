7cd6a142b7407304c2996cd6ae657d9c48795488
Danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-19 22:27:40 -0300

    Método para atualizar a lista de pagamento todo mês dia 15.

--- View/Usuariopadrao/Tela inicial/TelaPagamentoAlunos.cs
+++ View/Usuariopadrao/Tela inicial/TelaPagamentoAlunos.cs
@@ -11,23 +11,24 @@ namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
     public partial class TelaPagamentoAlunos : Form
     {
         private RepositorioPagamento alunoController;
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer(); 
 
         public TelaPagamentoAlunos()
         {
             InitializeComponent();
-            DatabaseService dbService = new DatabaseService(); 
-            alunoController = new RepositorioPagamento(dbService); 
+            DatabaseService dbService = new DatabaseService();
+            alunoController = new RepositorioPagamento(dbService);
         }
 
         private void TelaPagamentoAlunos_Load(object sender, EventArgs e)
         {
-            CarregarAlunos();   
+            CarregarAlunos();
+            ConfigurarTimerParaDiaEspecifico(15);  
         }
 
         private void CarregarAlunos()
         {
             List<Aluno> alunosAtivos = alunoController.AlunosAtivos();
-
             dataGridViewpagamento.DataSource = alunosAtivos;
 
             dataGridViewpagamento.CellFormatting += (s, ev) =>
@@ -36,16 +37,43 @@ namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
                 {
                     bool statusPagamento = (bool)dataGridViewpagamento.Rows[ev.RowIndex].Cells[ev.ColumnIndex].Value;
                     ev.CellStyle.BackColor = statusPagamento ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+                    ev.CellStyle.ForeColor = System.Drawing.Color.White; 
+                    //Ve se fica bom com branco!!!!!!!!!!!!!!!!!!!!!!!!
                 }
             };
+        }
+
+        private void ConfigurarTimerParaDiaEspecifico(int dia)
+        {
+            DateTime hoje = DateTime.Now;
+
+            DateTime proximoDia = new DateTime(hoje.Year, hoje.Month, dia);
+
+            if (hoje.Date > proximoDia.Date)
+            {
+                proximoDia = proximoDia.AddMonths(1);  
+            }
+
+            TimeSpan intervalo = proximoDia - hoje;
+
+            timer.Interval = (int)intervalo.TotalMilliseconds;
+            timer.Tick += Timer_Tick; 
+            timer.Start(); 
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+            CarregarAlunos();
+            MessageBox.Show("Lista de pagamentos atualizada!");
         }
 
         private void buttonPagamentoRealizado_Click(object sender, EventArgs e)
         {
             foreach (DataGridViewRow row in dataGridViewpagamento.SelectedRows)
             {
-                var aluno = (Aluno)row.DataBoundItem;  
-                alunoController.AtualizarStatusPagamento(aluno.Id, true);  
+                var aluno = (Aluno)row.DataBoundItem;
+                alunoController.AtualizarStatusPagamento(aluno.Id, true); 
             }
 
             CarregarAlunos();
@@ -53,10 +81,9 @@ namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
 
         private void buttonPendentePagamento_Click(object sender, EventArgs e)
         {
-            // Tenho que Atualizar o status de pagamento para 'false'  no banco de dados
             foreach (DataGridViewRow row in dataGridViewpagamento.SelectedRows)
             {
-                var aluno = (Aluno)row.DataBoundItem;  
+                var aluno = (Aluno)row.DataBoundItem;
                 alunoController.AtualizarStatusPagamento(aluno.Id, false);  
             }
 
@@ -64,15 +91,12 @@ namespace ProjetoIntegrador.View.Usuariopadrao.Tela_inicial
         }
 
         private void dataGridViewpagamento_Click(object sender, DataGridViewCellEventArgs e)
-        
         {
-            
             if (e.RowIndex >= 0)
             {
-                var aluno = (Aluno)dataGridViewpagamento.Rows[e.RowIndex].DataBoundItem; 
+                var aluno = (Aluno)dataGridViewpagamento.Rows[e.RowIndex].DataBoundItem;
                 MessageBox.Show($"Aluno selecionado: {aluno.Nome}, Status de Pagamento: {(aluno.StatusPagamento ? "Realizado" : "Pendente")}");
             }
         }
-
     }
 }
