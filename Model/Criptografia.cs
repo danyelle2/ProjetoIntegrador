69879b8d434e4776bad714c210f5f6f93fcde7f0
Danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-22 19:46:31 -0300

    Classe Criptografia criada

---
+++ Model/Criptografia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegrador.BancoDeDados
+{
+    public class Criptografia
+    {
+
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+        public static bool SecureEquals(string a, string b)
+        {
+            if (a.Length != b.Length) return false;
+
+            int result = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                result |= a[i] ^ b[i];
+            }
+
+            return result == 0;
+        }
+    }
+}
