8415ad25c39de7fee384d8e3adce48da53a3bde9
Danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-22 19:48:42 -0300

    Classe Usuario criada na pasta Model

--- Model/EntradaSaidaAlunos.cs
+++ Model/EntradaSaidaAlunos.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjetoIntegrador.Model
+﻿namespace ProjetoIntegrador.Controller.Aluno
 {
-    internal class EntradaSaidaAlunos
+    public class EntradaSaidaAlunos
     {
-        //ve se essa classe ainda vai ser util quando tiver o banco de dados
-        // ela é usada no gráfico mensal 
-        public string Mes { get; set; }
-        public int Entrada { get; set; }
-        public int Saida { get; set; }
+        public int Entradas { get; set; }
+        public int Saidas { get; set; }
     }
 }
