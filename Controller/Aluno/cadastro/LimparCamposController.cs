624d2858461ed7e61a2cf39b91dc5089d4920176
danyelle2 <dany.alvesbrito2@gmail.com>
2025-04-22 17:31:47 -0300

    LimparCampos ok

---
+++ Controller/Aluno/cadastro/LimparCamposController.cs
@@ -0,0 +1,30 @@
+﻿using ProjetoIntegrador.Controller.Aluno;
+using ProjetoIntegrador.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoIntegrador.Controller
+{
+    public class LimparCamposController
+    {
+        public bool LimparCampos(TextBox nome, TextBox idade, TextBox telefone, TextBox data, ComboBox plano, TextBox nomeResponsavel, ComboBox StatusAluno)
+        {
+
+            MessageBox.Show("Cadastro realizado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            nome.Clear();
+            idade.Clear();
+            telefone.Clear();
+            data.Clear();
+            plano.SelectedItem = null;
+            nomeResponsavel.Clear();
+            StatusAluno.SelectedItem = null;
+            return true;
+        }
+
+    }
+}
