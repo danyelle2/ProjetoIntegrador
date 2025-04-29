6bfadb3776dc75f7546d28a68cebe6d69f739205
Danyelle2 <danyelle.brito@senacsp.edu.br>
2025-04-28 21:31:52 -0300

    Alterada data de saida na classe aluno

--- Model/Aluno.cs
+++ Model/Aluno.cs
@@ -16,7 +16,8 @@ namespace ProjetoIntegrador.Model
         public int Idade { get; set; }
         public string Telefone { get; set; }
         public DateTime DataEntrada { get; set; }
-        public DateTime DataSaida { get; set; }
+        public DateTime? DataSaida { get; set; }
+
         public string NomeResponsavel { get; set; }
         public string Status { get; set; }
 
