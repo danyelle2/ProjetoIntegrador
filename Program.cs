a0ef1266b3a959d7de8967a9f634a92120df46ab
danyelle2 <danyelle.abrito@senacsp.edu.br>
2025-05-11 11:54:53 -0300

    Iniciar da maneira correta

--- Program.cs
+++ Program.cs
@@ -20,9 +20,8 @@ namespace ProjetoIntegrador
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
-            //Application.Run(new TelaLoadForms());
+            Application.Run(new TelaLoadForms());
             Application.Run(new TelaLoginForm());
-            //Application.Run(new TelaModalidadeEscolha());
         }
     }
 }
